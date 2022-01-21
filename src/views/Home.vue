ffe2e8835a77a2df70b9ec5913bf6f51b24fd57f
tsumugu <tsumugu1515@gmail.com>
2022-01-21 17:14:09 +0900

    fix: モバイルで画像ビュワーが崩れる問題を修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -261,8 +261,8 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   overflow: hidden;
   background: #4a4a4a;
   z-index: 999;
